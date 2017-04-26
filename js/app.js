b98f7196faa449b651a803e868b1ba12e459c0ae
Mario Terron <marioterron157@gmail.com>
2017-04-26 19:00:26 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Artists
+$('#search-artist').on('click', function(e) {
+    e.preventDefault()
+    var nameArtist = $('#artist-name').val()
+    var urlSearchArtist = 'https://api.spotify.com/v1/search?type=artist&query=' + nameArtist
+    $.ajax({
+        url: urlSearchArtist,
+        success: function(data) {
+            var artistsFound = data.artists.items
+            var optionsArtists = '<option selected disabled>Select an artist</option>'
+            artistsFound.forEach(function(artistData) {
+                optionsArtists += '<option value="' + artistData.id + '">' + artistData.name + '</option>'
+            })
+            $('#artists-selection').html(optionsArtists)
+        }
+    })
+})
+
+$('#artists-selection').on('change', function(e) {
+    var idArtist = $(this).val();
+    urlAlbums = 'https://api.spotify.com/v1/artists/' + idArtist + '/albums'
+    
+    $.ajax({
+        url: urlAlbums,
+        success: function(data) {
+            var albumsFound = data.items
+            var optionsAlbums = '<option selected disabled>Select an a track</option>'
+            albumsFound.forEach(function(albumData) {
+                optionsAlbums += '<option value="' + albumData.id + '">' + albumData.name + '</option>'
+            })
+            $('#albums-selection').html(optionsAlbums)
+        }
+    })
+
+})
+
+$('#albums-selection').on('change', function(e) {
+    var idAlbum = $(this).val();
+    urlTracks = 'https://api.spotify.com/v1/albums/' + idAlbum + '/tracks'
+    
+    $.ajax({
+        url: urlTracks,
+        success: function(data) {
+            var tracksFound = data.items
+            var listTracks = ''
+            tracksFound.forEach(function(trackData) {
+                listTracks += '<li class="list-group-item"><a href="'+ trackData.external_urls.spotify +'" target="_blank">' + trackData.name + '</a></li>'
+            })
+            $('#tracks-selection').html(listTracks)
+        }
+    })
+})
---
+++ readme.md
@@ -0,0 +1,10 @@
+# [Spotifinder](https://marioterron.github.io/spotifinder) #
+
+## Description ##
+
+This repository contains a Spotify webapp made with HTML, CSS and jQuery in the [Skylab Coders Academy](http://www.skylabcoders.com/es/)
+Full Stack Web Development Bootcamp.
+
+[Here](https://github.com/juanmaguitar/exercises-javascript/tree/master/04-jquery-spotify-API) you can see the description of the exercise and [here](https://marioterron.github.io/spotifinder) you can follow my progress.
+
+![Snapshot](img/full-site.jpg)
